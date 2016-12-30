49546cddd69011cbe78dcf4ea45856a03f7a0da2
Lars Gröber <larsgroeber7@gmail.com>
2016-12-30 16:52:24 +0100

    renamed files - fix

--- JAOGLL/src/jaogll_texture_cache.cpp
+++ JAOGLL/src/jaogll_texture_cache.cpp
@@ -8,7 +8,7 @@ GLTexture Texture_Cache::get_texture ( std::string texturePath )
     auto mit = _textureMap.find( texturePath );
     if ( mit == _textureMap.end() )
     {
-        GLTexture newTexture = jaogll_image_loader::loadPNG( texturePath );
+        GLTexture newTexture = Image_Loader::loadPNG( texturePath );
         _textureMap.insert( std::make_pair( texturePath, newTexture ) );
 
         std::cout << "New texture" << std::endl;
